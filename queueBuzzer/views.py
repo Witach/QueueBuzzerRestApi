49cd1fce746949ecb7681aae83a0536334a65cef
Przemhub <przegrabar@gmail.com>
2020-10-25 11:18:33 +0100

    Initialized project, created Models

---
+++ queueBuzzer/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Point, Product
+
+
+def index(request):
+    point = get_object_or_404(Point)
+    product = Product.objects.all()
+    context = {
+        'image':point.image,
+        'logo': point.logoImage,
+        "product":product,
+    }
+
+    return render(request,'queueBuzzer/index.html',context)
+# Create your views here.
